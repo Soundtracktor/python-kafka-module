adfa192f62e5aa1528b6ea69b4430f6326544456
Nicolai Seerup <mail@soundtracktor.com>
2022-12-29 17:58:38 +0100

    add code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-kafka-module
+
+python3 setup.py sdist
+
+https://github.com/MichaelKim0407/tutorial-pip-package
---
+++ soundtracktor_kafka/__init__.py
@@ -0,0 +1,28 @@
+from kafka import KafkaConsumer, KafkaProducer
+import os
+
+# Set up the Kafka consumer
+# value_deserializer=lambda x: _safe_utf8_decode(x),
+# move into python-kafka-module
+def create_consumer(*, topic, group_id)
+    return KafkaConsumer(
+        topic,  # topic to consume from
+        group_id=group_id,  # consumer group to join
+        bootstrap_servers=[os.environ.get('KAFKA_BROKER_0')],  # list of Kafka brokers
+        auto_offset_reset='earliest',  # start consuming from the earliest message
+        enable_auto_commit=True,  # disable auto-commit of offsets
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')), # decode the message value as UTF-8
+        key_deserializer=lambda x: x.decode('utf-8') if x else None,  # decode the message key as UTF-8
+        api_version=None,
+        max_poll_records=200,
+        session_timeout_ms=60000,
+    )
+
+# Set up the Kafka producer
+def create_producer(*, group_id)
+    return KafkaProducer(
+        group_id=group_id,
+        bootstrap_servers=[os.environ.get('KAFKA_BROKER_0')],  # list of Kafka brokers
+        value_serializer=lambda x: json.dumps(x).encode('utf-8'), # encode the message value as UTF-8
+        key_serializer=lambda x: x.encode('utf-8'),  # encode the message key as UTF-8
+    )
