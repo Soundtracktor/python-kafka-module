1139c3a4c285aec6a841edf7f0f77d00e39c1ff6
Nicolai Seerup <mail@soundtracktor.com>
2022-12-30 21:47:20 +0100

    remove json as dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='soundtracktor_kafka',
     version='1.0.1',
     packages=find_packages(),
-    install_requires=['kafka-python==2.0.2', 'json'],
+    install_requires=['kafka-python==2.0.2'],
 )
