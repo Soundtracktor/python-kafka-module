330f759cb67a8203ba2fce3609523ab37ba27fa2
Nicolai Seerup <mail@soundtracktor.com>
2023-01-13 16:29:38 +0100

    ensure int

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='soundtracktor_kafka',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=['kafka-python==2.0.2'],
 )
