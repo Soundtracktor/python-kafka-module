ec47bda540128bd52db2f03b2fa8df666d20bec7
Nicolai Seerup <mail@soundtracktor.com>
2023-01-13 16:16:58 +0100

    change defaults

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='soundtracktor_kafka',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     install_requires=['kafka-python==2.0.2'],
 )
